3f70252dc1b929afd8e028c5d36a7684c48bdcb9
Kater7 <Katerina.nki@mail.ru>
2020-05-19 16:13:05 +0300

    html, css, js and json file changes. Added getData.js file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
-const productList = document.getElementById('product-list');
+import { getData } from './getData.js';
+import { getMemoryInUnits } from './getMemoryInUnits.js';
+import { getPriceInWords } from './getPriceInWords.js';
 
-function getProdactsList(data) {
-    for (let i = 0; i < data.length; i++) {
-        productList.insertAdjacentHTML('beforeend', `
-            <div class="form">
-                <div class="title">${data[i].title}</div>
-                <div class="memory">Объем памяти: ${data[i].memory}</div>
-                <div class="price">Стоимость: ${data[i].price}руб.</div>
-            <div>
-    `);
+/**
+ * Create application
+ * @class
+ */
+class App {
+    constructor() {
+        this.prodactsList = [];
+    }
+
+    /** Application entry point */
+    async start() {
+        /**
+         * @global
+         * @type {object}
+         */
+        this.prodactsList = await getData('devices.json');
+
+        this.displayProdactsList(this.prodactsList);
+    }
+
+    /**
+     * Display prodacts list
+     * @param {object} data data array from json-file
+     */
+    displayProdactsList(data) {
+        /** Container for displaying prodacts list information */
+        const productList = document.getElementById('product-list');
+
+        for (let i = 0; i < data.length; i++) {
+            productList.insertAdjacentHTML('beforeend', `
+                <div class="form">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${data[i].title}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Объем памяти: ${data[i].memory}</h6>
+                            <h6 class="card-subtitle mb-2 text-muted">Стоимость: <span class="bold">${data[i].price}руб.</span></h6>
+                            <button type="button" class="btn btn-info">Купить</button>
+                        </div>
+                    </div>
+                <div>
+            `);
+        }
     }
 }
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'devices.json', true);
-xhr.onload = function() {
-    const data = JSON.parse(this.responseText);
-    console.log(data);
-    getProdactsList(data);
-}
-xhr.send(null);
+const app = new App(document.getElementById('app'));
+app.start()
